e79a435850f9a8ffca89f75e296bc87fe4b5fb40
huaxinrui <huaxinrui@bytedance.com>
2020-06-14 00:20:02 +0800

    X

--- src/main/java/org/apache/flink_sink/stream/App.java
+++ src/main/java/org/apache/flink_sink/stream/App.java
@@ -11,6 +11,7 @@ import java.util.Objects;
 public class App extends Stream{
 	public static void main(String[] args) {
 
+
 		StreamExecutionEnvironment env = getEnv();
 		DataStream<String> appStream = getStream();
 
